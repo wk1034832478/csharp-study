544d5f2545c033be44c061623b442ce081a2f149
王凯 <1034832478@qq.com>
2018-11-12 23:47:14 +0800

    update note

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.Text;
 namespace csharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "120";
+            int i = 0;
+            int.TryParse
+            if ( int.TryParse( str , out i ) ) {
+                Console.WriteLine( $"新的值：{i}" );
+            } else {
+                Console.WriteLine( $"{str}不是字符串" );
+            }
         }
     }
 }
